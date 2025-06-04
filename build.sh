b8af5eaacc23a658ae753e6aa8bcd50396f6d4bb
Prasanna Ramesh <114574731+Prasannaramesh13@users.noreply.github.com>
2025-06-04 11:17:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-DEV_IMAGE = 'prasanna1808/e-commerce'
-IMAGE_TAG = 'latest'
+DEV_IMAGE='prasanna1808/e-commerce'
+IMAGE_TAG='latest'
 
-echo "Building image with tag: $DEV_IMAGE:$IMAGE_TAG"
+echo "Building image with tag: ${DEV_IMAGE:$IMAGE_TAG}"
 
-docker build -t $DEV_IMAGE:$IMAGE_TAG .
+docker build -t ${DEV_IMAGE:$IMAGE_TAG} .
